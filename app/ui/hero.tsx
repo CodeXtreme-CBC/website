01223efe5fbefd24f3a921568c269e4f4d5981d5
CedricMurairi <murairicedric@gmail.com>
2025-11-03 16:10:39 +0200

    fixed font size issues

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -89,7 +89,7 @@ export default function Hero({ slides, title, tip, description}: HeroSliderProps
       </div>
       
       {/* Content overlay - outside of slides loop so it persists */}
-      <div className="tap absolute top-0 left-0 right-0 w-[100%] px-[5%] mx-auto h-full py-5 md:py-15 z-10 flex flex-col justify-between items-start">
+      <div className="tap absolute top-0 left-0 right-0 w-[100%] px-[5%] mx-auto h-full pb-5 pt-10 md:py-15 z-10 flex flex-col justify-between items-start">
         <FullNav />
         <div className='hidden lg:block'>
           <Link href="/">
